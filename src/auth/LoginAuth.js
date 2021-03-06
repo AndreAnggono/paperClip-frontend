4279722ea7da8338e591b57b635d70876134028c
Andre Anggono <andre.anggono@hotmail.com>
2021-03-06 16:36:38 +1100

    Removed debug code

--- src/auth/LoginAuth.js
+++ src/auth/LoginAuth.js
@@ -1,91 +1,74 @@
-import React, { Component } from 'react';
-import ErrorMessage from './ErrorMessage';
-import { SESSIONS } from '../auth/serverData';
-import axios from 'axios';
+import React, { Component } from "react";
+import ErrorMessage from "./ErrorMessage";
+import { SESSIONS } from "../auth/serverData";
+import axios from "axios";
 
 export class LoginAuth extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      username: '',
-      password: '',
-      error: '',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+		this.state = {
+			username: "",
+			password: "",
+			error: ""
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const { username, password } = this.state;
-    axios
-      .post(
-        SESSIONS,
-        {
-          user: {
-            username,
-            password,
-          },
-        },
-        { withCredentials: true }
-      )
-      .then((response) => {
-        console.log(response);
-        if (response.data.status === 401) {
-          this.setState({
-            error: response.data.error,
-            username: '',
-            password: '',
-          });
-        } else {
-          if (response.data.logged_in) {
-            this.props.handleSuccessfulAuth(response.data);
-          }
-        }
-      })
-      .catch((error) => {
-        console.log('login error', error);
-      });
-  }
+	handleSubmit(event) {
+		event.preventDefault();
+		const { username, password } = this.state;
+		axios
+			.post(
+				SESSIONS,
+				{
+					user: {
+						username,
+						password
+					}
+				},
+				{ withCredentials: true }
+			)
+			.then((response) => {
+				if (response.data.status === 401) {
+					this.setState({
+						error: response.data.error,
+						username: "",
+						password: ""
+					});
+				} else {
+					if (response.data.logged_in) {
+						this.props.handleSuccessfulAuth(response.data);
+					}
+				}
+			})
+			.catch((error) => {
+				console.log("login error", error);
+			});
+	}
 
-  render() {
-    return (
-      <>
-        <h1>Login</h1>
-        {/*<p>Test with user:<strong> ac</strong> and password:{' '}
+	render() {
+		return (
+			<>
+				<h1>Login</h1>
+				{/*<p>Test with user:<strong> ac</strong> and password:{' '}
           <strong>chicken</strong></p>*/}
-        {this.state.error.length > 1 && (
-          <ErrorMessage class={'error-box'} message={this.state.error} />
-        )}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            name='username'
-            placeholder='Username'
-            value={this.state.username}
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-          <button type='submit'>login</button>
-        </form>
-      </>
-    );
-  }
+				{this.state.error.length > 1 && <ErrorMessage class={"error-box"} message={this.state.error} />}
+				<form onSubmit={this.handleSubmit}>
+					<input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} required />
+					<input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+					<button type="submit">login</button>
+				</form>
+			</>
+		);
+	}
 }
 
 export default LoginAuth;
